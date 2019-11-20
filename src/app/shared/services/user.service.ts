d77b9ebe180f04de32fc15ffe44cf404f39e6e0e
RogersV <vincent1.rogers@gmail.com>
2019-11-20 13:12:00 -0500

    Login and Signup service with methods to connect to backend - #Not completed

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -7,15 +7,21 @@ import { HttpClient } from '@Angular/common/http';
 export class UserService {
 
   constructor(private httpClient: HttpClient) { }
+ 
+  // Create credentials object using username and password from parameters
+  // Pass credentials to url using http post method.
+  // Server returns data and we print it.
 
+  
   login(username: string, password: string){
 
     const credentials = {
       username: username, 
       password : password
     }
-    
-    const url = 'http://localhost:8080/user';
+
+  
+    const url = 'http://localhost:8080/user/login';
 
 
     this.httpClient.post(url, credentials)
@@ -23,4 +29,26 @@ export class UserService {
         console.log(data)
       })
   }
+
+  signup(FirstName: string, LastName: string, email: string, password: string ){
+
+    const SignUPcredentials = {
+      FirstName : FirstName,
+      LastName : LastName,
+      email : email,
+      password : password,
+      
+}
+
+  
+    const url = 'http://localhost:8080/user';
+
+
+    this.httpClient.post(url, SignUPcredentials)
+      .subscribe(data=>{
+        console.log(data)
+      })
+  }
+
+
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserService } from '../shared/services/user.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  
+  RegisterForm: FormGroup;
+  constructor(
 
-  ngOnInit() {
+    private formBuilder: FormBuilder,
+    private userservice : UserService
+  ) { }
+
+  submitted = false; 
+ ngOnInit() {
+
+    this.RegisterForm = this.formBuilder.group({
+      FirstName: '',
+      LastName: '',
+      email:'',
+      password: '',
+    })
+
+  }
+
+    onSubmit() {
+    this.submitted = true;
+
+    this.Registerfunction(
+      this.RegisterForm.value['FirstName'],
+      this.RegisterForm.value['LastName'],
+      this.RegisterForm.value['email'],
+      this.RegisterForm.value['password']);
+    }
+    async Registerfunction(FirstName : string, LastName: string, email: string, password : string){
+      this.userservice.signup(FirstName, LastName, email, password)
+ 
   }
 
 }
