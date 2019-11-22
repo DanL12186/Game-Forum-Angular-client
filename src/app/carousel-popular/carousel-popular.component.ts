baed0f314ecd8949d0974cb1aff32487f39c8df4
DanL12186 <DanL12186@users.noreply.github.com>
2019-11-22 10:01:11 -0500

    preliminary push-up of popular carousel populated w/gamedata
    Single-carousel a WIP; having serious issues with the multi-item
    carousel

--- src/app/carousel-popular/carousel-popular.component.ts
+++ src/app/carousel-popular/carousel-popular.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { GamePageService } from '../shared/services/game-page.service';
+import { GameData } from '../shared/modules/gameData';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-carousel-popular',
@@ -7,10 +10,27 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CarouselPopularComponent implements OnInit {
+  constructor(private gamePageService : GamePageService) { }
 
-  constructor() { }
+  public gameArray : GameData[];
+  public gameSubscription: Subscription;
+  public games: any = [[]];
+  
+  //resp
+  async getGames() {
+    this.gameSubscription = this.gamePageService.gamesArray
+      .subscribe(data => {
+        console.log('this is happening');
+        console.log(data);
+        this.gameArray = data;
+      });
+  }
 
   ngOnInit() {
+    this.getGames()
   }
 
 }
+
+
+
