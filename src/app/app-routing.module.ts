d1f0a01166f38458f98e7cfb5bc2adea2212cba9
RogersV <vincent1.rogers@gmail.com>
2019-11-22 11:37:40 -0500

    Working on user card halfway complete. Created Profile service.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { LoginComponent } from './shared/components/login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './shared/components/user-profile/user-profile.component';
@@ -10,28 +9,8 @@ const routes: Routes = [{
   redirectTo: '',
   pathMatch: 'full'
 }];
-=======
-import { GamesComponent } from './games/games.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { GameCardTableComponent } from './shared/components/game-card-table/game-card-table.component';
-import { MainPageComponent } from './shared/pages/main-page/main-page.component';
->>>>>>> a4379f37a4cd85e4d15a562f8b63f1006c86bea5
 
-const routes: Routes = [
-  { path: '', 
-    redirectTo: '/main-page', 
-    pathMatch: 'full' },
-  { path: 'main-page',
-    component: MainPageComponent},
-  { path: 'home',
-    component: HomePageComponent},
-  { path: 'games', 
-    component: GamesComponent},
-  { path: 'game-table',
-    component: GameCardTableComponent}
-];
 @NgModule({
-<<<<<<< HEAD
   imports:[ 
     RouterModule.forRoot([
     {
@@ -50,9 +29,6 @@ const routes: Routes = [
 
   ]),
   ],
-=======
-  imports:[RouterModule.forRoot(routes)],
->>>>>>> a4379f37a4cd85e4d15a562f8b63f1006c86bea5
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
