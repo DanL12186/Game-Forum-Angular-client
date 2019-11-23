7ab73f30c2d438f381504d5ae18f3dda8b06c792
SGeorges <SGeorges@github.com>
2019-11-22 19:54:16 -0500

    Took Vince's user sign-up and used mdbootstrap input form. Base format is off of his orientation -- Still no sign-up functionality tested. Will confirm

--- src/app/components/sign-up-card/sign-up-card.component.ts
+++ src/app/components/sign-up-card/sign-up-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+// ----- Services
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-sign-up-card',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up-card.component.scss']
 })
 export class SignUpCardComponent implements OnInit {
+  signupForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder : FormBuilder,
+    private userService : UserService
+  ) { 
+    this.signupForm = formBuilder.group({
+      signupFormFirstName: ['', Validators.required],
+      signupFormLastName: ['', Validators.required],
+      signupFormUsername: ['', Validators.required],
+      signupFormEmail: ['', [Validators.email, Validators.required]],
+      signupFormPassword: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  register() {
+    console.log("Registering");
   }
 
 }
