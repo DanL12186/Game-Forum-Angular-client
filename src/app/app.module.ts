03fa73e86a8839e871084d1526d136b7f08020da
Kevin <Kevin_Saephanh@yahoo.com>
2019-11-24 17:20:46 -0500

    Notes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
 
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { WavesModule, TableModule } from 'angular-bootstrap-md';
+import { MDBBootstrapModule } from "angular-bootstrap-md";
+import { WavesModule, TableModule } from "angular-bootstrap-md";
 
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { AppComponent } from './app.component';
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { AppComponent } from "./app.component";
 
-import { LoginComponent } from './shared/components/login/login.component';
-import { RouterModule } from '@angular/router';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { CarouselNewComponent } from './carousel-new/carousel-new.component';
-import { CarouselPopularComponent } from './carousel-popular/carousel-popular.component';
-import { GameCardTableComponent } from './shared/components/game-card-table/game-card-table.component';
-import { MainPageComponent } from './shared/pages/main-page/main-page.component';
-import { GameDetailComponent } from './game-detail/game-detail.component';
-import { SearchResultsComponent } from './search-results/search-results.component';
-import { CommentComponent } from './comment/comment.component';
-
+import { LoginComponent } from "./shared/components/login/login.component";
+import { RouterModule } from "@angular/router";
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { CarouselNewComponent } from "./carousel-new/carousel-new.component";
+import { CarouselPopularComponent } from "./carousel-popular/carousel-popular.component";
+import { GameCardTableComponent } from "./shared/components/game-card-table/game-card-table.component";
+import { MainPageComponent } from "./shared/pages/main-page/main-page.component";
+import { GameDetailComponent } from "./game-detail/game-detail.component";
+import { SearchResultsComponent } from "./search-results/search-results.component";
+import { CommentComponent } from "./comment/comment.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -44,9 +44,10 @@ import { CommentComponent } from './comment/comment.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     MDBBootstrapModule.forRoot()
-    ],
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/pages/main-page/main-page.component.ts
+++ src/app/shared/pages/main-page/main-page.component.ts
@@ -1,19 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { GamePageService } from '../../services/game-page.service';
+import { Component, OnInit } from "@angular/core";
+import { GamePageService } from "../../services/game-page.service";
 
 @Component({
-  selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  selector: "app-main-page",
+  templateUrl: "./main-page.component.html",
+  styleUrls: ["./main-page.component.scss"]
 })
 export class MainPageComponent implements OnInit {
-
-  constructor(
-    private gamePageService : GamePageService
-  ) { }
+  constructor(private gamePageService: GamePageService) {}
 
   ngOnInit() {
     this.gamePageService.getGame();
   }
-
 }
